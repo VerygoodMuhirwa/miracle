252682fc6bca5946c5f31c17489f3b488b5c7edc
MUHIRWA Verygood <121917699+VerygoodMuhirwa@users.noreply.github.com>
2023-07-27 06:17:04 +0200

    Update verifyToken.js

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -1,8 +1,8 @@
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
-​
+
 dotenv.config();
-​
+
 module.exports.createToken = async (user) => {
     let tokenAge = 60 * 60 * 24 * 10;
     const token = await jwt.sign(user.toJSON(), process.env.SECRET_KEY, {
@@ -10,13 +10,13 @@ module.exports.createToken = async (user) => {
     });
     return token;
 };
-​
+
 module.exports.isAuth = (req, res, next) => {
     const authorization = req.headers.authorization;
 // console.log(authorization);
     if (authorization) {
         const token1 = authorization.split(" ");
-        const token=token1[1]
+        const token = token1.split(" ")[1];
         
         jwt.verify(token, process.env.SECRET_KEY, (err, decode) => {
             if (err) {
